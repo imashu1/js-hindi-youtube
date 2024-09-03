c25f81fc0a019cb73942382758eeaa9a97b4ffc6
imashu1 <121037559+imashu1@users.noreply.github.com>
2024-09-03 06:30:26 +0000

    objects and array

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -9,7 +9,7 @@ const dc_heros = ["superman", "flash", "batman"]
 // const allHeros = marvel_heros.concat(dc_heros)
 // console.log(allHeros);
 
-const all_new_heros = [...marvel_heros, ...dc_heros]
+const all_new_heros = [...marvel_heros, ...dc_heros] // spread out values because of spread operator
 
 // console.log(all_new_heros);
 
